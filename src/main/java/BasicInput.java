396b301363e0347d7e79cb832859c6d0cce34f9e
Narany1 <malikforch01@gmail.com>
2025-01-18 00:55:23 -0500

    Complete. charAt() function can return the character at a specific index of a string.

--- src/main/java/BasicInput.java
+++ src/main/java/BasicInput.java
@@ -11,12 +11,17 @@ public class BasicInput {
 
         System.out.println("Enter integer:");
         userInt = scnr.nextInt();
-
         // TODO (1): Finish reading other items into variables, then output the four values on a single line separated by a space
-
+        System.out.println("Enter double:");
+        userDouble = scnr.nextDouble();
+        System.out.println("Enter character:");
+        userChar = scnr.next().charAt(0);
+        System.out.println("Enter string:");
+        userString = scnr.next();
+        System.out.println(userInt + " " + userDouble + " " + userChar + " " + userString);
         // TODO (2): Output the four values in reverse
-
+        System.out.println(userString + " " + userChar + " " + userDouble + " " + userInt);
         // TODO (3): Cast the double to an integer, and output that integer
-
+        System.out.println(userDouble + " cast to an integer is " + (int) userDouble);
     }
 }
